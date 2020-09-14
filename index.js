667c7a8faae5a6b0799ef275cad409c6384ff2f2
Erik Duxstad <eduxstad@gmail.com>
2020-09-14 15:06:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //node.js - purdue-covid19
-//acts as an api for confirming email addresses
+//api for confirming email addresses
 
 const express = require('express');
 const app = express();
@@ -10,8 +10,23 @@ app.get('/', function (req, res) {
 
 app.get('/signup', function (req, res) {
  let email = req.query.email;
+ //check if the request seems valid
+ //check if the email has already been requested
+ //check if the email is already on the list
+ //send the signup email and add the random key to the map
+ //make sure to delete key after 15 minutes
+ //redirect user to successful signup page
  res.send(email);
 });
+
+app.get('/confirm', function (req, res) {
+ let email = req.query.email;
+ //check if the request matches the random key
+ //add the user to the mailing list
+ //redirect user to the confirm page
+ res.send(email);
+});
+
 
 console.log("Starting . . .");
 app.listen(process.env.PORT);
