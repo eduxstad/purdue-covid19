6d712ef4b9d8407f5324bf6f176874192f0c6997
Erik Duxstad <eduxstad@gamil.com>
2020-09-14 14:39:01 -0400

    add express and backend

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 //node.js - purdue-covid19
 //acts as an api for confirming email addresses
 
+const express = require('express');
+const app = express();
+ 
+app.get('/', function (req, res) {
+ res.send('Hello World')
+});
+
 console.log("Starting . . .");
+app.listen(3000);
